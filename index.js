c4c42e17e563e24b4a71c00e623c6bd3b5858ed2
Peter Ndungu <petersonndungu995@gmail.com>
2024-04-01 17:32:24 +0300

    “completed”

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+console.log(returnFirstTwoDrivers(drivers)); 
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+// selectingDrivers array
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier function
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return fare * integer;
+    };
+};
+
+// fareDoubler function
+const fareDoubler = createFareMultiplier(2);
+
+// fareTripler function
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers function
+const selectDifferentDrivers = function(drivers, selectionFunction) {
+    return selectionFunction(drivers);
+};
+
+
